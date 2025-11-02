25ccc82bc951a4e1a19bd9c2bb2ab66df6f6e8bb
Riley <rileyjordan21@hotmail.com>
2025-11-02 23:02:45 +0000

    attempt2

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // API slice
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL || "" }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",

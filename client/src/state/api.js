46bdae9dcddbf8c1f69ca0ece0521ffdedba0540
Riley <rileyjordan21@hotmail.com>
2025-11-03 13:58:45 +0000

    backend working, now connecting cors

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // API slice
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL || "" }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: "https://mern-saas-project.vercel.app"  // Hardcoded for now
+  }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
